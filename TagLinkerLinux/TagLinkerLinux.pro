ce2eaa5ff593fd52468eb07b7087474fd74db70e
Agustin Perez Paladini <aguperezpala@gmail.com>
2016-06-26 22:31:25 +0200

    adding the linux test version, we will refactor/remove this later

---
+++ TagLinkerLinux/TagLinkerLinux.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-04-11T20:39:49
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = TagLinkerLinux
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        MainWindow.cpp
+
+HEADERS  += MainWindow.h
+
+FORMS    += mainwindow.ui
---
+++ TagLinkerLinux/main.cpp
@@ -0,0 +1,21 @@
+/*
+ *******************************************************************************
+ * TEMPLATE_TO_BE_CHANGED_BEGIN
+ *   Copyright (C) 2016 by Happen
+ *
+ *   File main.cpp created by agustin on 11.4.2016
+ * TEMPLATE_TO_BE_CHANGED_END
+ *******************************************************************************
+ */
+
+#include "MainWindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
